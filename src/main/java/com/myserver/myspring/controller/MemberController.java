7cdccc5081fdcc36f40dc30a66a00cf9fa00e982
loinsir <a9327370@gmail.com>
2021-01-29 13:47:19 +0000

    회원 가입 폼 테스트 까지

--- src/main/java/com/myserver/myspring/controller/MemberController.java
+++ src/main/java/com/myserver/myspring/controller/MemberController.java
@@ -1,10 +1,15 @@
 package com.myserver.myspring.controller;
 
+import java.util.List;
+
+import com.myserver.myspring.domain.Member;
 import com.myserver.myspring.service.MemberService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -16,5 +21,24 @@ public class MemberController {
         this.memberService = memberService;
     }
     
-    
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/"; //홈 화면으로 보내버리기
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
