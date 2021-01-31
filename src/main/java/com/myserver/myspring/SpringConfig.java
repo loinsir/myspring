49adbfc53313fe3ab6c7872bf06097235c316e98
loinsir <a9327370@gmail.com>
2021-01-31 07:02:13 +0000

    끔찍한  jdbc 추가

--- src/main/java/com/myserver/myspring/SpringConfig.java
+++ src/main/java/com/myserver/myspring/SpringConfig.java
@@ -1,14 +1,25 @@
 package com.myserver.myspring;
 
+import javax.sql.DataSource;
+
+import com.myserver.myspring.repository.JdbcMemberRepository;
 import com.myserver.myspring.repository.MemberRepository;
-import com.myserver.myspring.repository.MemoryMemberRepository;
+// import com.myserver.myspring.repository.MemoryMemberRepository;
 import com.myserver.myspring.service.MemberService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;  //spring이 관리하는 dataSource
+
+    @Autowired //di
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     
     @Bean
     public MemberService memberService() {
@@ -17,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
